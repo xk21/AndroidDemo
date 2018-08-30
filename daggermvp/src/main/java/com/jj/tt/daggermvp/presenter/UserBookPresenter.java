a78e6c85bbaae2b0e21ddfda082c819c620a8991
chenmingying <15815593025@163.com>
2018-08-30 16:08:30 +0800

    对mode的处理

--- daggermvp/src/main/java/com/jj/tt/daggermvp/presenter/UserBookPresenter.java
+++ daggermvp/src/main/java/com/jj/tt/daggermvp/presenter/UserBookPresenter.java
@@ -2,6 +2,7 @@ package com.jj.tt.daggermvp.presenter;
 
 
 import android.content.Context;
+import android.util.Log;
 
 import com.jj.tt.daggermvp.Componet.DaggerUserBookPresenterComponet;
 import com.jj.tt.daggermvp.base.BasePresenter;
@@ -20,29 +21,24 @@ public class UserBookPresenter extends BasePresenter<IUserBookShowListener> {
 
     @Inject
     UserBookMode mUserBookMode;
-    private IUserBookShowListener iUserBookShowListener;
-
-    public UserBookPresenter( Context iUserBookShowListener) {
-        this.iUserBookShowListener = obtainView();
+    public UserBookPresenter( Context aaa) {
         DaggerUserBookPresenterComponet.create().inject(this);
+        Log.d("szjjyh","getUserBookSucce11ss="+obtainView());
+
     }
 
     public void getUserBook(final int i) {
         mUserBookMode.getUserBook(i, new OnUserBookListener() {
             @Override
             public void getUserBookSuccess(Book book) {
-                if (iUserBookShowListener==null){
-                    return;
-                }
-                iUserBookShowListener.getUserBookSuccess(book);
+                Log.d("szjjyh","getUserBookSuccess=");
+                obtainView().getUserBookSuccess(book);
             }
 
             @Override
             public void getUserBookFailed(String book) {
-                if (iUserBookShowListener==null){
-                    return;
-                }
-                iUserBookShowListener.getUserBookFailed(book);
+                Log.d("szjjyh","getUserBookFailed=");
+                obtainView().getUserBookFailed(book);
             }
         });
     }
